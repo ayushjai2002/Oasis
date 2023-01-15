02d179e5cb38413f936765e71331709f07025c58
ayushjai2002 <92423812+ayushjai2002@users.noreply.github.com>
2023-01-15 17:29:19 +0530

    Update AtmMachine.java

--- AtmMachine.java
+++ AtmMachine.java
@@ -1,6 +1,6 @@
-package ankit;
+package ayush;
 import java .util.*;
-public class atmmachine {
+public class AtmMachine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
